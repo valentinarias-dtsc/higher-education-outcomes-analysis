95a647719237d7cb2784ed8b4ae43e564b084a34
Valentin <valentinarias425@gmail.com>
2026-05-21 12:48:30 -0300

    Add new metric columns for metric_construction step

--- src/config/columns.py
+++ src/config/columns.py
@@ -27,3 +27,26 @@ OFFERING_TEXT_COLS = [
     "delivery_mode",
 ]
 
+SUM_COLS = {
+    "completion_count": ["promoted_completion_count", "regular_completion_count"],
+    "attrition_count": ["dropout_count", "free_status_count"],
+    "adverse_outcomes_count": ["dropout_count", "insufficient_count", "free_status_count"]
+}
+
+METRIC_COMPONENT_COLS = ENROLLMENT_METRIC_COMPONENT_COLS + [
+    "completion_count",
+    "attrition_count",
+    "adverse_outcomes_count",
+]
+
+
+NEW_RATE_METRIC_COLS = [
+    "dropout_rate",
+    "insufficient_rate",
+    "free_status_rate",
+    "promoted_completion_rate",
+    "regular_completion_rate",
+    "completion_rate",
+    "attrition_rate",
+    "adverse_outcomes_rate",
+]
